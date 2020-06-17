a342246581a83b52e5d89109dde1a256ff9b33b8
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-06-17 11:53:59 +0900

    デザインの調整

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <html>
-<head><title>掲示板App</title></head>
+<head>
+    <title>掲示板App</title>
+    <meta name="viewport" content="width=device-width" inital-scale=1>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
 <body>
 
-<h1>掲示板App</h1>
+<h1 style="color: red;">掲示板App</h1>
 
 <h2>投稿フォーム</h2>
 
