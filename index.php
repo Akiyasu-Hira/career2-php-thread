0b5c60e58239d916fb11f5312071cb51a96314b3
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-07-01 12:24:55 +0900

    クラス分け

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php
+
+/**
+ * 職業実践2 - 掲示板アプリ
+ */
+
+session_start();
+
+function setToken()
+{
+    $token = sha1(uniqid(mt_rand(), true));
+    $_SESSION['token'] = $token;
+}
+
+function checkToken()
+{
+    if (empty($_SESSION['token'])) {
+        echo "Sessionが空です";
+        exit;
+    }
+
+    if (($_SESSION['token']) !== $_POST['token']) {
+        echo "不正な投稿です。";
+        exit;
+    }
+
+    $_SESSION['token'] = null;
+}
+
+if (empty($_SESSION['token'])) {
+    setToken();
+}
+?>
+
 <html>
 <head>
     <title>掲示板App</title>
@@ -29,70 +63,23 @@
 date_default_timezone_set( "Asia/Tokyo" );
 const THREAD_FILE = 'thread.txt';
 
-function readData() {
-    // ファイルが存在しなければデフォルト空文字のファイルを作成する
-    if (! file_exists(THREAD_FILE)) {
-        $fp = fopen(THREAD_FILE, 'w');
-        fwrite($fp, '');
-        fclose($fp);
+require_once './Thread.php';
+$thread = new Thread('掲示板App');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["method"]) && $_POST["method"] === "DELETE") {
+        $thread->delete();
+    } else {
+        $thread->post($_POST['personal_name'], $_POST['contents']);
     }
-
-    $thread_text = file_get_contents(THREAD_FILE);
-    echo $thread_text;
-}
-
-function writeData() {
-    $personal_name = $_POST['personal_name'];
-    $contents = $_POST['contents'];
-    $contents = nl2br($contents);
-
-    $data = "<hr>\n";
-    $data = $data."<p>投稿日時：".date("Y/m/d G:i:s")."</p>\n";
-    $data = $data."<p>投稿者:".$personal_name."</p>\n";
-    $data = $data."<p>内容:</p>\n";
-    $data = $data."<p>".$contents."</p>\n";
-
-    $fp = fopen(THREAD_FILE, 'a');
-
-    if ($fp){
-        if (flock($fp, LOCK_EX)){
-            if (fwrite($fp,  $data) === FALSE){
-                print('ファイル書き込みに失敗しました');
-            }
-
-            flock($fp, LOCK_UN);
-        }else{
-            print('ファイルロックに失敗しました');
-        }
-    }
-
-    fclose($fp);
-
-}
-    
-
-    function deleteData() {
-        file_put_contents(THREAD_FILE,"");
-    }
-
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        if (isset($_POST["method"]) && $_POST["method"] === "DELETE") {
-            deleteData();
-        } else {
-            writeData();
-        }
 
     //ブラウザのリロード対策
     $redirect_url = $_SERVER['HTTP_REFERER'];
-    header("Location:$redirect_url");
+    header("Location: $redirect_url");
     exit;
 }
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    writeData();
-}
-
-readData();
+echo $thread->getList();
 
 ?>
 
